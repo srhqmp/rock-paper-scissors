1b7c577a247ae04cc5d37192f2a7cf5f769f542a
srhqmp <ballanosarah@gmail.com>
2024-08-18 21:05:51 +0800

    feat: implement computer choice

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log("Hello world");
+
+const choices = ["Rock", "Paper", "Scissors"];
+
+// randomly returns rock paper or scissors
+function getComputerChoice() {
+  const randomNum = Math.floor(Math.random() * 3);
+  return choices[randomNum];
+}
+
+// console.log(getComputerChoice());
