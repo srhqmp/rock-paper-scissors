96955ba4c25c3bd80a8fa52f5654b0009b789055
srhqmp <ballanosarah@gmail.com>
2024-08-18 22:05:17 +0800

    feat: write the logic to play a single round

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-console.log("Hello world");
-
 const choices = ["Rock", "Paper", "Scissors"];
+let humanScore = 0;
+let computerScore = 0;
 
 // randomly returns rock paper or scissors
 function getComputerChoice() {
@@ -19,8 +19,54 @@ function getHumanChoice() {
         choice.toLocaleLowerCase() === input.trim().toLocaleLowerCase()
     );
 
-  if (!choice) getHumanChoice();
+  if (!choice) {
+    return getHumanChoice();
+  }
   return choice;
 }
 
-console.log(getHumanChoice());
+function determineScore(humanSelection, computerSelection) {
+  //   tie
+  if (humanSelection === computerSelection) {
+    return { computer: 0, human: 0 };
+  }
+  //   win
+  if (
+    (humanSelection === choices[0] && computerSelection === choices[2]) ||
+    (humanSelection === choices[1] && computerSelection === choices[0]) ||
+    (humanSelection === choices[2] && computerSelection === choices[1])
+  ) {
+    return { human: 1, computer: 0 };
+  }
+  //   lose
+  return { human: 0, computer: 1 };
+}
+
+function logMessage(humanSelection, computerSelection, score) {
+  let message;
+
+  if (score.computer === score.human) {
+    message = `It's a tie! You both chose ${humanSelection}.`;
+  } else if (score.computer > score.human) {
+    message = `You lose! ${computerSelection} beats ${humanSelection}.`;
+  } else {
+    message = `You win! ${humanSelection} beats ${computerSelection}.`;
+  }
+
+  console.log(message);
+}
+
+// play a single round
+function playRound(humanSelection, computerSelection) {
+  //   determine winner
+  const score = determineScore(humanSelection, computerSelection);
+  //   announce winner
+  logMessage(humanSelection, computerSelection, score);
+  //   increment score of winner
+  humanScore = score.human ? (humanScore += 1) : humanScore;
+  computerScore = score.computer ? (computerScore += 1) : computerScore;
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+playRound(humanSelection, computerSelection);
